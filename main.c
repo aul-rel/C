6f39f913074e0b9f0dd1de19e5ddf987b8249617
Aurellio <aurellio@outlook.com.br>
2025-02-18 10:51:50 -0300

    Update main.c

--- main.c
+++ main.c
@@ -11,18 +11,20 @@ Code, Compile, Run and Debug online from anywhere in world.
 int main()
 {
     int idade = 30;
-    int aniversario = 15;
+    int idade2 = 15;
     float altura = 1.75;
     double peso = 70.5;
-    float nota = 7.8;
     char inicial = 'A';
+    float nota = 7.86;
     
     printf("Hello World\n");//Comentário, até o final da linha
     printf("Idade: %d anos\n", idade);//%d - para representar numero inteiro
-    printf("Altura: %.2f metros\n", altura);//%f - para representar numero decimal
+    printf("Altura: %.2f metros\n", altura);//%f - para representar numero decimal, . seguido de casa representa a casa decimal ou critério de arredondamento
     printf("Peso: %.1f kg\n", peso);
-    printf("Nota: %.2f\n", nota);
     printf("Inicial do nome: %c\n", inicial);
-
+    nota = 8.5;
+    printf("Nota: %.1f\n", nota);
+    printf("\nVocê tem %d anos e sua nota foi %.1f.\n", idade, nota);
+    
     return 0;
 }
