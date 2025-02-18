579cf3a0bd04d3546a762948f8a3796d93623a02
Aurellio <aurellio@outlook.com.br>
2025-02-18 10:29:10 -0300

    Update main.c

--- main.c
+++ main.c
@@ -14,12 +14,14 @@ int main()
     int aniversario = 15;
     float altura = 1.75;
     double peso = 70.5;
+    float nota = 7.8;
     char inicial = 'A';
     
     printf("Hello World\n");//Comentário, até o final da linha
     printf("Idade: %d anos\n", idade);//%d - para representar numero inteiro
     printf("Altura: %.2f metros\n", altura);//%f - para representar numero decimal
     printf("Peso: %.1f kg\n", peso);
+    printf("Nota: %.2f\n", nota);
     printf("Inicial do nome: %c\n", inicial);
 
     return 0;
