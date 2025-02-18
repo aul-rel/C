87e12df22e00345a5a670a45e71e77615d7a1f3d
Aurellio <aurellio@outlook.com.br>
2025-02-18 10:26:01 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,1 +1,26 @@
-main
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int idade = 30;
+    int aniversario = 15;
+    float altura = 1.75;
+    double peso = 70.5;
+    char inicial = 'A';
+    
+    printf("Hello World\n");//Comentário, até o final da linha
+    printf("Idade: %d anos\n", idade);//%d - para representar numero inteiro
+    printf("Altura: %.2f metros\n", altura);//%f - para representar numero decimal
+    printf("Peso: %.1f kg\n", peso);
+    printf("Inicial do nome: %c\n", inicial);
+
+    return 0;
+}
